9c3780fb2bbc2a77aab00ce49179f119573d80bb
Cesar flores <cesar.flores5761@alumnos.udg.mx>
2025-11-12 07:02:41 -0800

    cambios pruebas

---
+++ pollotv/DatosUsuarioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DatosUsuarioViewController.swift
+//  pollotv
+//
+//  Created by ALUMNOS on 05/11/25.
+//
+
+import UIKit
+
+class DatosUsuarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
